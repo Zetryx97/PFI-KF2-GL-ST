75b4f8a8cf11ed74425ee30ca2b194cc12177309
Zetryx97 <gablessard17@gmail.com>
2022-12-01 11:28:35 -0500

    started testing movement

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from moteur import Moteur
 
 
 
-radio_navig = Radionavigation()
+#radio_navig = Radionavigation()
 
 
-radio_navig.show_position_robot()
-sleep(1)
-radio_navig.fermer_port_radionavigation()
-sleep(1)
+#radio_navig.show_position_robot()
+#sleep(1)
+#radio_navig.fermer_port_radionavigation()
+#sleep(1)
+
+moteur_robot = Moteur()
+
+
+moteur_robot .avancer(0.4)
+sleep(2)
+moteur_robot .freiner()
+sleep(2)
+moteur_robot .tourner_droite(0.7)
+sleep(3)
